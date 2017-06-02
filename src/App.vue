51eaeb15fe1309e42cf00047eaafee3cd2e21b95
Andy Crockett <andy@crockett.io>
2017-06-02 09:00:39 -0500

    playing with docs design

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
                     <img src="./assets/logo.png" class="logo">
                     <h1 class="header-title">
                         Vuex<span class="lighter">Forms</span>
-                        <span class="version">(ALPHA 0.0.1)</span>
+                        <span class="version">v0.0.1 (alpha)</span>
                     </h1>
                 </div>
                 <p class="slogan">
@@ -32,26 +32,24 @@
                     <div class="columns">
                         <div class="column is-6">
                             <ul class="feature-list">
-                                <li>Built for Vuex - works without it</li>
-                                <li>Model based forms & validation</li>
-                                <li>Unobstructive presentation solution</li>
-                                <li>Prebuilt components, easily restyled</li>
-                                <li>Quick custom components from Mixins</li>
-                                <li>Simple input masking</li>
-                                <li>Configurable behavior</li>
+                                <li>Model based forms & validations</li>
+                                <li>Dispatches a Vuex action on submit</li>
+                                <li>Configurable for AJAX instead of Vuex</li>
+                                <li>Loosely-coupled form components</li>
+                                <li>Minimal logic in components</li>
+                                <li>Mixins for quick custom components</li>
+                                <li>Easy to use input masking</li>
                             </ul>
                         </div>
                         <div class="column is-6">
                             <ul class="feature-list">
-                                <li>
-                                    Validation powered by
-                                    <a href="https://monterail.github.io/vuelidate/">Vuelidate</a>
+                                <li>Robust validation, powered by <a href="https://monterail.github.io/vuelidate/">Vuelidate</a>
                                 </li>
                                 <li>20+ <a href="https://laravel.com/">Laravel</a> inspired validation rules</li>
                                 <li>Template-decoupled validation messages</li>
-                                <li>Client-side validation message localization</li>
-                                <li>Handles server-side validation messages</li>
                                 <li>Custom validation rules & messages</li>
+                                <li>Localization for messages</li>
+                                <li>Configurable behavior</li>
                                 <li>
                                     Minimal dependencies
                                     <br/>
@@ -69,20 +67,50 @@
 
         <div class="docs">
             <div class="navigation-pane">
-                <a href="#" class="nav-title">Getting Started</a>
+                <a href="#getting-started" class="nav-title">Getting Started</a>
                 <ul class="nav-list">
-                    <li>Introduction</li>
-                    <li>Installation</li>
-                    <li>Basic Usage</li>
+                    <li><a href="#introduction">Introduction</a></li>
+                    <li><a href="#installation">Installation</a></li>
+                    <li><a href="#basic-usage">Basic Usage</a></li>
+                </ul>
+                <a href="#validation" class="nav-title">Validation</a>
+                <ul class="nav-list">
+                    <li><a href="#basic-validations">Basic Validations</a></li>
+                    <li><a href="#dynamic-validations">Dynamic Validations</a></li>
                 </ul>
             </div>
             <div class="content-area">
+                <a class="anchor" name="getting-started"></a>
                 <h1>Getting Started</h1>
-                <h2>Basic Validation Example</h2>
-                <form-example></form-example>
-                <hr/>
-                <h2>Dynamic Validation Example</h2>
-                <form-example2></form-example2>
+                <section class="doc-section">
+                    <div class="doc-example">
+                        <a class="anchor" name="introduction"></a>
+                        <h2>Introduction</h2>
+                    </div>
+                    <div class="doc-example">
+                        <a class="anchor" name="installation"></a>
+                        <h2>Installation</h2>
+                    </div>
+                    <div class="doc-example">
+                        <a class="anchor" name="basic-usage"></a>
+                        <h2>Basic Usage</h2>
+                    </div>
+                </section>
+
+                <a class="anchor" name="validation"></a>
+                <h1>Validation</h1>
+                <section class="doc-section">
+                    <div class="doc-example">
+                        <a class="anchor" name="basic-validations"></a>
+                        <h2>Basic Validation</h2>
+                        <form-example></form-example>
+                    </div>
+                    <div class="doc-example">
+                        <a class="anchor" name="dynamic-validations"></a>
+                        <h2>Dynamic Validation</h2>
+                        <form-example2></form-example2>
+                    </div>
+                </section>
                 <router-view></router-view>
             </div>
         </div>
@@ -106,183 +134,4 @@
 
 <style lang="scss">
     @import "./sass/main.scss";
-
-    $vue-light: #41b993;
-    $vue-dark: #2c3e50;
-    $saturated-vue-light: lighten(saturate($vue-light, 50), 35);
-    $saturated-vue-dark: lighten(saturate($vue-dark, 25), 45);
-
-    @keyframes fadeIn {
-        0% {
-            opacity: 0
-        }
-        100% {
-            opacity: 1
-        }
-    }
-
-    .header {
-        position: relative;
-        &:before {
-            content: ' ';
-            display: block;
-            position: absolute;
-            z-index: -1;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background: linear-gradient(to right bottom, $saturated-vue-light 0%, $saturated-vue-dark 100%);
-            opacity: 0;
-            animation: fadeIn 4s ease-out forwards;
-        }
-        color: #2c3e50;
-        padding: 60px 1rem;
-        text-align: center;
-        font-size: 1.25rem;
-        min-height: 100vh;
-        a {
-            color: saturate(darken($vue-light, 20), 50);
-            &:hover {
-                color: #FFF;
-            }
-        }
-    }
-
-    .header-centered {
-        display: block;
-        width: 100%;
-        @media (min-width: 700px) and (min-height: 730px) {
-            position: absolute;
-            top: 50%;
-            left: 0;
-            right: 0;
-            transform: translateY(-50%);
-        }
-    }
-
-    .header-logo {
-        margin-bottom: 4rem;
-    }
-
-    .logo {
-        display: inline-block;
-        vertical-align: middle;
-        height: calc(8vw + 6vh);
-        max-height: 120px;
-    }
-
-    .header-title {
-        color: $vue-dark;
-        display: inline-block;
-        vertical-align: middle;
-        font-weight: 700;
-        font-size: calc(16px + 4vw + 3vh);
-        font-variant: all-small-caps;
-        margin: 0;
-        .lighter {
-            color: saturate(darken($vue-light, 15), 40);
-        }
-    }
-
-    .version {
-        display: block;
-        text-align: right;
-        font-size: 1rem;
-        font-variant: normal;
-    }
-
-    .slogan {
-        color: darken($vue-dark, 25);
-        font-size: 1.1em;
-    }
-
-    .features {
-        color: darken($vue-dark, 10);
-        text-align: left;
-        width: 100%;
-        max-width: 800px;
-        margin: 4rem auto;
-        font-size: 0.82em;
-    }
-
-    .features-title {
-        font-weight: 500;
-    }
-
-    .feature-list {
-        margin: 0;
-        padding: 0;
-        & > li {
-            display: block;
-            margin: 0.5rem 1rem;
-            & > ul {
-                display: block;
-                margin: 0.25rem 0 0 0.5rem;
-                padding: 0;
-                & > li {
-                    display: block;
-                    margin: 0.1rem 1rem;
-                }
-            }
-        }
-    }
-
-    .docs {
-        width: 100%;
-        font-size: 0;
-    }
-
-    .navigation-pane {
-        display: inline-block;
-        vertical-align: top;
-        width: 250px;
-        height: 100vh;
-        border-right: 1px solid #ccc;
-        padding: 1rem;
-        font-size: 1rem;
-        @media (max-width: 768px) {
-            display: none;
-        }
-    }
-
-    .content-area {
-        display: inline-block;
-        vertical-align: top;
-        width: calc(100% - 250px);
-        padding: 2rem;
-        font-size: 1rem;
-    }
-
-    .nav-title {
-        font-size: 1.25em;
-    }
-
-    .nav-list {
-        li {
-            display: block;
-        }
-    }
-
-    .api-reports {
-        position: fixed;
-        top: 0;
-        right: 1rem;
-    }
-
-    .api-status {
-        margin-top: 1rem;
-        padding: 2rem;
-        border-radius: 1rem;
-        background: #41b883;
-        color: #FFF;
-    }
-
-    .api-error {
-        margin-top: 1rem;
-        padding: 2rem;
-        border-radius: 1rem;
-        background: #b83146;
-        color: #FFF;
-    }
 </style>
